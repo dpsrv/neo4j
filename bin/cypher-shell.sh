76529c3a5ef279d6349ef8239564fc7533be8c63
Max <max@maxf.net>
2026-08-02 21:50:45 -0400

    updated

--- bin/cypher-shell.sh
+++ bin/cypher-shell.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-NEO4J_URI="${NEO4J_URI:-bolt://localhost:7687}"
-NEO4J_USER="${NEO4J_USER:-neo4j}"
-NEO4J_PASSWORD="${NEO4J_PASSWORD:-changeme}"
+NEO4J_AUTH=$(kubectl get secret neo4j-auth -n dpsrv -o jsonpath='{.data.neo4j-auth}' | base64 -d)
+NEO4J_USER="${NEO4J_AUTH%%/*}"
+NEO4J_PASSWORD="${NEO4J_AUTH#*/}"
 
 if [ -n "$1" ]; then
   echo "$1" | kubectl exec -i -n dpsrv deploy/neo4j -- cypher-shell -u "$NEO4J_USER" -p "$NEO4J_PASSWORD"
